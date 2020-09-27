7d0b53f773557c72429d4de523eef2759bcbc135
liuzan <>
2020-09-27 12:59:18 +0800

    completed login

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
-import './assets/css/global.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+// 全局样式
+import "./assets/css/global.css";
+// 字体图标
+import "./assets/fonts/iconfont.css";
 
-Vue.config.productionTip = false
+import axios from "axios";
+// 配置请求的跟路径
+axios.defaults.baseURL = "http://119.23.53.78:8888/api/private/v1/";
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
