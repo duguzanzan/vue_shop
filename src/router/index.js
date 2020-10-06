0c8e9fe6eaa1c3f2e37fb77296ba0949d725d9c9
liuzan <>
2020-10-07 01:17:22 +0800

    completed rights files

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from "../components/Login.vue";
 import Home from "../components/Home.vue";
 import Welcome from "../components/Welcome.vue";
 import Users from "../components/User/Users.vue";
+import Rights from "../components/power/Rights.vue";
+import Roles from "../components/power/Roles.vue";
 
 Vue.use(VueRouter);
 
@@ -23,6 +25,8 @@ const routes = [
     children: [
       { path: "/welcome", component: Welcome },
       { path: "/users", component: Users },
+      { path: "/rights", component: Rights },
+      { path: "/roles", component: Roles },
     ],
   },
 ];
